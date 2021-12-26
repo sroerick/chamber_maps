8639e7679dd9520910291d30f8898aa6cda51536
Roerick <sweeney@roerick.me>
2021-12-26 09:28:29 -0800

    modify models and load script with state tiger data

--- county/load.py
+++ county/load.py
@@ -1,33 +1,58 @@
 import os
 from django.contrib.gis.utils import LayerMapping
-from county.models import countyMap
+from county.models import countyMap, stateTiger
 
 county_mapping = {
-'statefp' : 'STATEFP',
-'countyfp' : 'COUNTYFP',
-'countyfp' : 'COUNTYFP',
-'countyns' : 'COUNTYNS',
-'geoid' : 'GEOID',
-'name' : 'NAME',
-'namelsad' : 'NAMELSAD',
-'lsad' : 'LSAD',
-'classfp' : 'CLASSFP',
-'mtfcc' : 'MTFCC',
-'csafp' : 'CSAFP',
-'cbsafp' : 'CBSAFP',
-'metdivfp' : 'METDIVFP',
-'funcstat' : 'FUNCSTAT',
-'aland' : 'ALAND', 
-'awater' : 'AWATER', 
-'intptlat' : 'INTPTLAT',
-'intptlon' : 'INTPTLON',
-'geometry' : 'MULTIPOLYGON',
+    'statefp' : 'STATEFP',
+    'countyfp' : 'COUNTYFP',
+    'countyfp' : 'COUNTYFP',
+    'countyns' : 'COUNTYNS',
+    'geoid' : 'GEOID',
+    'name' : 'NAME',
+    'namelsad' : 'NAMELSAD',
+    'lsad' : 'LSAD',
+    'classfp' : 'CLASSFP',
+    'mtfcc' : 'MTFCC',
+    'csafp' : 'CSAFP',
+    'cbsafp' : 'CBSAFP',
+    'metdivfp' : 'METDIVFP',
+    'funcstat' : 'FUNCSTAT',
+    'aland' : 'ALAND', 
+    'awater' : 'AWATER', 
+    'intptlat' : 'INTPTLAT',
+    'intptlon' : 'INTPTLON',
+    'geometry' : 'MULTIPOLYGON',
 }
+
+statefips_mapping = {
+    'region': 'REGION',
+    'division': 'DIVISION',
+    'statefp': 'STATEFP',
+    'statens': 'STATENS',
+    'geoid': 'GEOID',
+    'stusps': 'STUSPS',
+    'name': 'NAME',
+    'lsad': 'LSAD',
+    'mtfcc': 'MTFCC',
+    'funcstat': 'FUNCSTAT',
+    'aland': 'ALAND',
+    'awater': 'AWATER',
+    'intptlat': 'INTPTLAT',
+    'intptlon': 'INTPTLON',
+    'geom': 'MULTIPOLYGON',
+}
+
 
 county_shp = os.path.abspath(
         os.path.join(os.path.dirname(__file__), 'data', 'tl_2021_us_county.shp'),
 )
 
+state_shp = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), 'data', 'tl_2017_us_state.shp'),
+)
+
 def run(verbose=True):
-    lm = LayerMapping(countyMap, county_shp, county_mapping, transform=True)
-    lm.save(strict=True, verbose=verbose)
+    county_lm = LayerMapping(countyMap, county_shp, county_mapping, transform=True)
+    countylm.save(strict=True, verbose=verbose)
+    state_lm = LayerMapping(countyMap, county_shp, county_mapping, transform=True)
+    statelm.save(strict=True, verbose=verbose)
--- county/models.py
+++ county/models.py
@@ -35,6 +35,23 @@ class countyMap(models.Model):
     def __str__(self):
         return self.name
 
+class stateTiger(models.Model):
+    region = models.CharField(max_length=2)
+    division = models.CharField(max_length=2)
+    statefp = models.CharField(max_length=2)
+    statens = models.CharField(max_length=8)
+    geoid = models.CharField(max_length=2)
+    stusps = models.CharField(max_length=2)
+    name = models.CharField(max_length=100)
+    lsad = models.CharField(max_length=2)
+    mtfcc = models.CharField(max_length=5)
+    funcstat = models.CharField(max_length=1)
+    aland = models.BigIntegerField()
+    awater = models.BigIntegerField()
+    intptlat = models.CharField(max_length=11)
+    intptlon = models.CharField(max_length=12)
+    geom = models.MultiPolygonField(srid=4326)
+
 
 class countyData(models.Model):
     county=models.CharField(max_length=100)
