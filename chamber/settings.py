c259d198172a8d1c60244ab2a5a37f13603c4421
Roerick <sweeney@roerick.me>
2022-01-11 18:44:20 -0800

    remove django heroku

--- chamber/settings.py
+++ chamber/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-import django_heroku
+#import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -144,4 +144,4 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 DATA_UPLOAD_MAX_MEMORY_SIZE = 350000000
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 1000000
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
