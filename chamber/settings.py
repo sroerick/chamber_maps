dde0f5ddde716dd2af09ccf2b35759a98b56547d
Roerick <sweeney@roerick.me>
2022-01-11 23:17:45 -0800

    change url for static files

--- chamber/settings.py
+++ chamber/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/chamber/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 #import ipdb; ipdb.set_trace()
 
