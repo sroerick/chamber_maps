f5f5a52d76fbec3291f6ccfe25559b96ec99fa0f
Roerick Sweeney <sweeney@roerick.me>
2022-01-12 16:43:16 -0600

    import djdatabaseurl

--- chamber/settings.py
+++ chamber/settings.py
@@ -90,6 +90,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
